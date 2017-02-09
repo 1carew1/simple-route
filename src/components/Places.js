35e97e3f7a5948c7b9bb79bf9da324ff94bf42ad
Colm Carew <colmcarew2@gmail.com>
2017-02-09 22:14:26 +0000

    Adding Map and Places component

---
+++ src/components/Places.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Places extends Component {
+  render() {
+    return (
+      <div>
+      	Places Component
+      </div>
+    );
+  }
+}
+
+export default Places;
