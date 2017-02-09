c5234324b0cfd85267427ed2b2f8b6ffa72eea19
Colm Carew <colmcarew2@gmail.com>
2017-02-09 23:16:36 +0000

    Editing lat lngs center

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Places from './components/Places';
 
 class App extends Component {
   render() {
-    const center = {
-      lat : 0,
-      lng : 0
+    const location = {
+      lat : 40.7575285,
+      lng : -73.9884469
     };
 
     return (
@@ -21,7 +21,7 @@ class App extends Component {
           <Places/>
         </p>
        <div style={{width:500, height:600, background:'red'}}>
-          <Map center={center}/>
+          <Map center={location}/>
       </div>
       </div>
     );
