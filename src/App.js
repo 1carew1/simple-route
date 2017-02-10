3a0b1e44c99594ce10d360ae9c184ff950c0edf0
Colm Carew <colmcarew2@gmail.com>
2017-02-10 20:18:14 +0000

    Using Superagent to perform calls to Google Geocoding

--- src/App.js
+++ src/App.js
@@ -3,8 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 import Map from './components/Map';
 import Places from './components/Places';
+import superagent from 'superagent';
 
 class App extends Component {
+  componentDidMount() {
+    console.log('componentDidMount')
+
+    const url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=40.7575285,-73.9884469&key=AIzaSyCHqxdyNpGyEZJAIgXJP-lrQzabxk92GqQ'
+    // Run Superage to get API Requests e.g. Google Maps Geocoding
+    superagent
+    .get(url)
+    .query(null)
+    .set('Accept', 'text/json')
+    .end((error, response) => {
+        console.log(JSON.stringify(response.body));
+    })
+  }
+
   render() {
     const location = {
       lat : 40.7575285,
