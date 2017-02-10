001fccb602a69be975ddf6ebff87e7cecf27e867
Colm Carew <colmcarew2@gmail.com>
2017-02-10 19:10:55 +0000

    Adding Google Maps Library to index.html

--- src/index.js
+++ src/index.js
@@ -2,12 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-// import 'https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap';
 
-//TODO : Need to import google maps library!
-//    <script async defer
-    // type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap">
-    // </script>
 ReactDOM.render(
   <App />,
   document.getElementById('root')
