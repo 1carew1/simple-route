36f786800cd8922233bc2a605caa8eacea54d0c6
Colm Carew <colmcarew2@gmail.com>
2017-02-20 22:30:17 +0000

    Maps only shown if logged in

--- src/App.js
+++ src/App.js
@@ -3,52 +3,8 @@ import { Router } from 'react-router';
 
 
 import './App.css';
-import Map from './components/Map';
-import Places from './components/Places';
-import superagent from 'superagent';
 
 class App extends Component {
-  componentDidMount() {
-    // console.log('componentDidMount')
-
-    //Address to Lat Lng
-
-    //https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCHqxdyNpGyEZJAIgXJP-lrQzabxk92GqQ
-
-    //Lat Lng to Address
-
-    //https://maps.googleapis.com/maps/api/geocode/json?latlng=40.7575285,-73.9884469&key=AIzaSyCHqxdyNpGyEZJAIgXJP-lrQzabxk92GqQ
-
-
-    const url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=40.7575285,-73.9884469&key=AIzaSyCHqxdyNpGyEZJAIgXJP-lrQzabxk92GqQ'
-    // Run Superagent to get API Requests e.g. Google Maps Geocoding
-    superagent
-    .get(url)
-    .query(null)
-    .set('Accept', 'text/json')
-    .end((error, response) => {
-        // console.log(JSON.stringify(response.body));
-        const results = response.body.results;
-        // console.log(results);
-        const addresses = results.map((obj, i) => {
-            return obj.formatted_address;
-        });
-        console.log('componentDidMount : Got ' + addresses.length + ' addresses from Google');
-
-        const latlngs = results.map((obj, i) => {
-            const latlng = obj.geometry.location;
-            return latlng;
-        });
-        console.log('componentDidMount : Got ' + latlngs.length + ' latlngs from Google');
-        localStorage.setItem('latlngs', JSON.stringify(latlngs));
-        localStorage.setItem('addresses', JSON.stringify(addresses));
-
-        // Reload the page
-        this.setState({});
-        // console.log(addresses);
-    })
-  }
-
   static contextTypes = {
     router: PropTypes.object
   }
@@ -67,26 +23,14 @@ class App extends Component {
   }
 
   render() {
-    let location = {
-      lat : 40.7575285,
-      lng : -73.9884469
-    };
-    const addresses = JSON.parse(localStorage.getItem('addresses') || '[]');
-    const latlngs = JSON.parse(localStorage.getItem('latlngs') || '[]');
     return (
       <div className="App">
-         <div style={{ height: '100%' }}>
-             {this.content}
-         </div>
          <div className="App-header">
             <h2>Welcome to Simple Route</h2>
          </div>
-         <div style={{width:500, height:600, margin:10}}>
-            <Map center={location} markers={latlngs}/>
-        </div>
-        <div>
-          <Places addresses={addresses}/>
-      </div>
+         <div style={{ height: '100%' }}>
+             {this.content}
+         </div>
       </div>
     );
   }
--- src/views/Main/Home/Home.js
+++ src/views/Main/Home/Home.js
@@ -3,7 +3,53 @@ import {Button} from 'react-bootstrap'
 import AuthService from '../../../utils/AuthService'
 import styles from './styles.module.css'
 
+import Map from '../../../components/Map';
+import Places from '../../../components/Places';
+
+import superagent from 'superagent';
+
 export class Home extends React.Component {
+    componentDidMount() {
+    // console.log('componentDidMount')
+
+    //Address to Lat Lng
+
+    //https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCHqxdyNpGyEZJAIgXJP-lrQzabxk92GqQ
+
+    //Lat Lng to Address
+
+    //https://maps.googleapis.com/maps/api/geocode/json?latlng=40.7575285,-73.9884469&key=AIzaSyCHqxdyNpGyEZJAIgXJP-lrQzabxk92GqQ
+
+
+    const url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=40.7575285,-73.9884469&key=AIzaSyCHqxdyNpGyEZJAIgXJP-lrQzabxk92GqQ'
+    // Run Superagent to get API Requests e.g. Google Maps Geocoding
+    superagent
+    .get(url)
+    .query(null)
+    .set('Accept', 'text/json')
+    .end((error, response) => {
+        // console.log(JSON.stringify(response.body));
+        const results = response.body.results;
+        // console.log(results);
+        const addresses = results.map((obj, i) => {
+            return obj.formatted_address;
+        });
+        console.log('componentDidMount : Got ' + addresses.length + ' addresses from Google');
+
+        const latlngs = results.map((obj, i) => {
+            const latlng = obj.geometry.location;
+            return latlng;
+        });
+        console.log('componentDidMount : Got ' + latlngs.length + ' latlngs from Google');
+        localStorage.setItem('latlngs', JSON.stringify(latlngs));
+        localStorage.setItem('addresses', JSON.stringify(addresses));
+
+        // Reload the page
+        this.setState({});
+        // console.log(addresses);
+    })
+  }
+
   static contextTypes = {
     router: T.object
   }
@@ -28,12 +74,28 @@ export class Home extends React.Component {
   }
 
   render(){
+    let location = {
+      lat : 40.7575285,
+      lng : -73.9884469
+    };
+    const addresses = JSON.parse(localStorage.getItem('addresses') || '[]');
+    const latlngs = JSON.parse(localStorage.getItem('latlngs') || '[]');
     const { profile } = this.state
     return (
+    <div>
       <div className={styles.root}>
         <h2>Home</h2>
         <p>Welcome {profile.name}!</p>
         <Button onClick={this.logout.bind(this)}>Logout</Button>
+      </div>
+      <div>
+         <div style={{width:500, height:600, margin:10}}>
+              <Map center={location} markers={latlngs}/>
+          </div>
+          <div>
+            <Places addresses={addresses}/>
+        </div>
+      </div>
       </div>
     )
   }
