c2b6187c2ed8b755ac86f18c3ffc85c429cd7662
Colm Carew <colmcarew2@gmail.com>
2017-03-02 21:40:59 +0000

    Adding in a Modal - Need to put this somewhere better

--- src/views/Main/Navigation/CustomNavbar.js
+++ src/views/Main/Navigation/CustomNavbar.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react'
-import { Nav, Navbar, NavDropdown, MenuItem, NavItem } from 'react-bootstrap';
+import { Nav, Navbar, NavDropdown, MenuItem, NavItem, Modal, Button, OverlayTrigger, Popover, Tooltip } from 'react-bootstrap';
 import {Link} from 'react-router'
 
 // create classes
 class CustomNavbar extends Component {
-  
+
+  constructor() {
+  	super();
+    this.state = { showModal: false };
+  }
+
   generateLink(link) {
   	let navItemName = link.text;
 	if(link.linkTo) {
@@ -12,6 +17,17 @@ class CustomNavbar extends Component {
 	}
 	return navItemName;
   }
+
+
+  close() {
+    this.setState({ showModal: false });
+  }
+
+  open() {
+  	console.log('Opening Modal');
+    this.setState({ showModal: true });
+  }
+
 
   centerMap() {
 	const location = JSON.parse(localStorage.getItem('currentLocation') || '{}');
@@ -30,12 +46,12 @@ class CustomNavbar extends Component {
 	myNavBarData.links = [
 	  {linkTo: "/home", text: "Home"},
 	  {linkTo: "/about", text: "About"},
+	  {linkTo: "/logout", text: "Logout"},
 	  {disabled:mapOptionsDisabled, dropdown: true, text: "Map Options", links: [
 	    {text: "Centre Map", onClick: this.centerMap.bind(this)},
-	    {text: "Fly to Location", onClick: function(){console.log('Flying to Location')}},
+	    {text: "Fly to Location", onClick: this.open.bind(this)},
 	    {text: "Get Directions", onClick: function(){console.log('Getting Directions')}}
-	  ]}, 
-	  {linkTo: "/logout", text: "Logout"}
+	  ]}
 	];
 	let navItems = null;
 	if(myNavBarData.links) {
@@ -64,7 +80,41 @@ class CustomNavbar extends Component {
 		})
 	}
 
+	const popover = (
+      <Popover id="modal-popover" title="popover">
+        very popover. such engagement
+      </Popover>
+    );
+    const tooltip = (
+      <Tooltip id="modal-tooltip">
+        wow.
+      </Tooltip>
+    );
     return(
+    	<div>
+	    	<Modal show={this.state.showModal} onHide={this.close.bind(this)}>
+	          <Modal.Header closeButton>
+	            <Modal.Title>Modal heading</Modal.Title>
+	          </Modal.Header>
+	          <Modal.Body>
+	            <h4>Text in a modal</h4>
+	            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+
+	            <h4>Popover in a modal</h4>
+	            <p>there is a <OverlayTrigger overlay={popover}><a href="#">popover</a></OverlayTrigger> here</p>
+
+	            <h4>Tooltips in a modal</h4>
+	            <p>there is a <OverlayTrigger overlay={tooltip}><a href="#">tooltip</a></OverlayTrigger> here</p>
+
+	            <hr />
+
+	            <h4>Overflowing text to show scroll behavior</h4>
+	            <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+	          </Modal.Body>
+	          <Modal.Footer>
+	            <Button onClick={this.close.bind(this)}>Close</Button>
+	          </Modal.Footer>
+	        </Modal>
 		  <Navbar fluid inverse fixedTop collapseOnSelect>
 		    <Navbar.Header>
 		      <Navbar.Brand>
@@ -78,6 +128,7 @@ class CustomNavbar extends Component {
 		      </Nav>
 		    </Navbar.Collapse>
 		  </Navbar>
+		</div>
 	);
   }
 }
