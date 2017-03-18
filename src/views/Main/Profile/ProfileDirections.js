2d20648d30ab4f467e8adf9c88562ba47f68a96b
Colm Carew <colmcarew2@gmail.com>
2017-03-18 15:48:18 +0000

    Making last 10 Searched Directions Viewable

--- src/views/Main/Profile/ProfileDirections.js
+++ src/views/Main/Profile/ProfileDirections.js
@@ -1,20 +1,32 @@
 import React from 'react';
-import {Jumbotron} from 'react-bootstrap';
+import {Jumbotron, Table} from 'react-bootstrap';
 import FirebaseDatabaseService from '../../../utils/FirebaseDatabaseService';
+import StringUtils from '../../../utils/StringUtils';
+import {Link} from 'react-router';
 
 import './jumbostyle.css';
 
 const firebaseDatabaseService = new FirebaseDatabaseService();
+const stringUtils = new StringUtils();
 
-let someList = null;
+let tableContents = null;
 export class ProfileDirections extends React.Component {
 
   componentDidMount(){
     let functionToRunOnResults = (results) => {
 
       if(results) {
-       someList = results.map((value, index) => {
-          return <li key={index}>{value.start_address} - TO - {value.end_address} - On - {value.date_searched}</li>;
+       // Ensure the results are in order of most recent descending
+       tableContents = results.reverse().map((value, index) => {
+          const linkToDirections = 'home/directions/' + value.start_address + '/' + value.end_address;
+          return (
+                  <tr key={index}>
+                    <td><Link to={linkToDirections} >{index + 1}</Link></td>
+                    <td>{value.start_address}</td>
+                    <td>{value.end_address}</td>
+                    <td>{stringUtils.pretifyDateString(value.date_searched)}</td>
+                  </tr>
+            );
         });      
       } 
       this.setState({});
@@ -25,8 +37,20 @@ export class ProfileDirections extends React.Component {
   render() {
     return (
       <Jumbotron className="centerJumbo">
-          <h3>Recently Searched Directions :</h3>
-          <ul>{someList}</ul>
+          <h3>Last 10 Searched Directions :</h3>
+          <Table striped bordered condensed hover style={{marginLeft : '10%', width:'80%', marginRight:'10%'}}>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Start Address</th>
+                <th>End Address</th>
+                <th>Search Time</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tableContents}
+            </tbody> 
+          </Table>
       </Jumbotron>
     )
   }
